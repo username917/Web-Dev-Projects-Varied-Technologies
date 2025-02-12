af04da6a2ec773cc7b0dd341ff7a60e0cf195d43
givanov88@hotmail.com <givanov@DESKTOP-RIMGLLH>
2025-02-12 00:07:17 -0500

    Feb 11 2025
    Added logging; to be ehhanced later; soem refactoring on Payment Service
    and Controller

--- Spring-Boot-Concepts/src/main/java/com/payment_system/controller/PaymentController.java
+++ Spring-Boot-Concepts/src/main/java/com/payment_system/controller/PaymentController.java
@@ -1,5 +1,8 @@
 package com.payment_system.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,32 +11,55 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.payment_system.service.PaymentGateway;
+import com.payment_system.service.PaymentService;
 
 
 @RestController
 @RequestMapping("/payment")
 public class PaymentController {
 	
+	/*
 	private final PaymentGateway creditCardPayment;
 	private final PaymentGateway paypalPayment;
 	private final PaymentGateway bankTransferPayment;
+	*/
 	
+	private final Map<String, PaymentGateway> paymentGateways;
+	private final PaymentService paymentService;
 	@Autowired
 	public PaymentController(
+				PaymentService paymentService,
 				@Qualifier("creditCardPaymentGateway") PaymentGateway creditCardPayment,
 				@Qualifier("paypalPaymentGateway") PaymentGateway paypalPayment,
 				@Qualifier("bankTransferPaymentGateway") PaymentGateway bankTransferPayment
 			) {
+		/*
 		this.creditCardPayment = creditCardPayment;
 		this.bankTransferPayment = bankTransferPayment;
 		this.paypalPayment = paypalPayment;
-	
+		*/
+		this.paymentService = paymentService;
+		 this.paymentGateways = new HashMap<>();
+	     this.paymentGateways.put("creditcard", creditCardPayment);
+	     this.paymentGateways.put("paypal", paypalPayment);
+	     this.paymentGateways.put("banktransfer", bankTransferPayment);
 	}
 	
 	@GetMapping("/process")
 	public String processPayment(@RequestParam String method) {
 		
+		PaymentGateway gateway = paymentGateways.get(method.toLowerCase());
+		
+		if (gateway == null) {
+			return "Invalid payment method. Choose from one of the following: " + paymentGateways.keySet();
+		}
+		
+		return paymentService.processPayment(gateway);
+		
+		
+		/*
 		switch (method.toLowerCase()) {
+		
 		
 		case "creditcard":
 			return creditCardPayment.processPayment();
@@ -47,6 +73,7 @@ public class PaymentController {
 		default:
 			return "Invalid payment method. Choose a credit card, paypal or a bank transfer";
 		}
+		*/
 	}
 	
 	
--- Spring-Boot-Concepts/src/main/java/com/payment_system/service/PaymentService.java
+++ Spring-Boot-Concepts/src/main/java/com/payment_system/service/PaymentService.java
@@ -1,20 +1,21 @@
 package com.payment_system.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
 	
-	private final PaymentGateway paymentGateway;
+	private static final Logger logger = LoggerFactory.getLogger(PaymentService.class);
 	
-	public PaymentService(@Qualifier("creditCardPaymentGateway") PaymentGateway paymentGateway) {
-		
-		this.paymentGateway = paymentGateway;
-	}
-	
-	public String processPayment() {
-		return paymentGateway.processPayment();
-	}
-
+	public String processPayment(PaymentGateway paymentGateway) {
+        logger.info("Processing payment with: {}", paymentGateway.getClass().getSimpleName());
+        return paymentGateway.processPayment();
+    }
 }
