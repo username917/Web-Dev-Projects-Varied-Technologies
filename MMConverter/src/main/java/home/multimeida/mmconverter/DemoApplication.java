4fec181f387ea4187d76c0b45eee5c6f06493eb2
givanov88@hotmail.com <givanov@DESKTOP-RIMGLLH>
2025-05-13 18:58:34 -0400

    Test commit

--- MMConverter/src/main/java/home/multimeida/mmconverter/DemoApplication.java
+++ MMConverter/src/main/java/home/multimeida/mmconverter/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
 	 public static void main(String[] args) {
 		 
-		 	//staging a change.
+		 	//staging a change. test transaction.
 		
 	        // Disable headless mode
 	        System.setProperty("java.awt.headless", "false");
