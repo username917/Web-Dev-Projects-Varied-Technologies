25013d6dd3ae6f17e322b49db8042de3cec80afc
givanov88@hotmail.com <givanov@DESKTOP-RIMGLLH>
2025-05-13 18:02:06 -0400

    May 13 2025
    Initial push of Walk-In Clinic sample Capstone project

---
+++ Walk-In-Clinic-System/src/main/java/com/walkinclinic/Models/Doctor.java
@@ -0,0 +1,97 @@
+package com.walkinclinic.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "doctors")
+public class Doctor {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "doctorid")
+	private int doctorid;
+	
+	@Column(name = "fName")
+	private String fName;
+	
+	@Column(name = "lName")
+	private String lName;
+	
+	@Column(name = "specialty")
+	private String specialty;
+	
+	@Column(name = "availability")
+	private String availability;
+	
+	@Column(name = "contactinfo")
+	private String contactinfo;
+	
+	@Column(name = "education")
+	private String education;
+
+	public int getDoctorid() {
+		return doctorid;
+	}
+
+	public void setDoctorid(int doctorid) {
+		this.doctorid = doctorid;
+	}
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+
+	public String getAvailability() {
+		return availability;
+	}
+
+	public void setAvailability(String availability) {
+		this.availability = availability;
+	}
+
+	public String getContactinfo() {
+		return contactinfo;
+	}
+
+	public void setContactinfo(String contactinfo) {
+		this.contactinfo = contactinfo;
+	}
+
+	public String getEducation() {
+		return education;
+	}
+
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	
+	
+	
+	
+
+}
---
+++ Walk-In-Clinic-System/src/main/java/com/walkinclinic/WalkInClinicSystemApplication.java
@@ -0,0 +1,13 @@
+package com.walkinclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkInClinicSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkInClinicSystemApplication.class, args);
+	}
+
+}
