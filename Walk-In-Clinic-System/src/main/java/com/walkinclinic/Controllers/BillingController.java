488631dc0289918f1df188d844d860eead6bcc93
givanov88@hotmail.com <givanov@DESKTOP-RIMGLLH>
2025-06-03 22:48:22 -0400

    June 3 2025
    Corrected errors to allow project compilation to WAR format and
    deployment.
    
    See pom.xml build/plugins section for more details on the eirslett
    plugin for the purpose.
    
    Node/npm will be updated to LTS versions at a later stage.

--- Walk-In-Clinic-System/src/main/java/com/walkinclinic/Controllers/BillingController.java
+++ Walk-In-Clinic-System/src/main/java/com/walkinclinic/Controllers/BillingController.java
@@ -1,5 +1,8 @@
 package com.walkinclinic.Controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BillingController {
 
 }
---
+++ Walk-In-Clinic-System/src/test/java/com/walkinclinic/WalkInClinicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walkinclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = WalkInClinicSystemApplication.class)
+public class WalkInClinicSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // just checking Spring Boot can load the context
+    }
+}
