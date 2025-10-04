ba705b26a41426049490e334ee7228df61f54518
givanov88@hotmail.com <givanov@DESKTOP-RIMGLLH>
2025-10-04 00:31:49 -0400

    OCt 3 2025
    Testing a commit from a transatlantic flight.

---
+++ Walk-In-Clinic-System/src/main/java/com/walkinclinic/Controllers/PrescriptionController.java
@@ -0,0 +1,20 @@
+package com.walkinclinic.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PrescriptionController {
+	
+	@PreAuthorized("hasRole('ROLE_ADMIN")
+	
+	// @PreAuthorized("hasRole('ROLE_ADMIN")
+	
+	// @PreAuthorized("hasRole('ROLE_ADMIN")
+	
+	// @PreAuthorized("hasRole('ROLE_ADMIN")
+	
+	
+
+}
---
+++ Walk-In-Clinic-System/src/main/java/com/walkinclinic/DTO/PrescriptionDTO.java
@@ -0,0 +1,41 @@
+package com.walkinclinic.DTO;
+
+public class PrescriptionDTO {
+    private String drugName;
+    private String dosage;
+    private String frequency;
+    private String duration;
+    private String notes;
+	public String getDrugName() {
+		return drugName;
+	}
+	public void setDrugName(String drugName) {
+		this.drugName = drugName;
+	}
+	public String getDosage() {
+		return dosage;
+	}
+	public void setDosage(String dosage) {
+		this.dosage = dosage;
+	}
+	public String getFrequency() {
+		return frequency;
+	}
+	public void setFrequency(String frequency) {
+		this.frequency = frequency;
+	}
+	public String getDuration() {
+		return duration;
+	}
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+    
+    
+}
